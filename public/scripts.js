a6fb99508b602ecf3656e0a3a0a17cdeda86513f
DanniFV <gitignore>
2026-01-29 16:42:33 +0000

    Úr tímum

--- public/scripts.js
+++ public/scripts.js
@@ -1,1 +1,39 @@
 /* útfæra */
+
+const correctElement = document.querySelector('counter .correct');
+const incorrectElement = document.querySelector('counter .incorrect');
+
+if (!correctElement || !incorrectElement) {
+    console.error('unable to find elements')
+}
+
+function questionAnswerListener(e) {
+    const button = e.target;
+
+    const isCorrect = button.classList.contains('button-correct')
+    const parentQuestion = button.closest(`.question`)
+    //Todo disable á takka
+
+    console.log(button.parentQuestion)
+
+    if (!correctElement) {
+        throw new Error('Missing correct Element')
+    }
+
+    if (isCorrect) {
+        const currentCorrectText = correctElement?.textContent;
+        const currentCorrect = Number.parseInt(currentCorrectText ?? '0');
+
+        const updatedCorrect = currentCorrect + 1;
+
+        correctElement.textContent = updatedCorrect.toString;
+
+    }
+    
+}
+
+const buttons = document.querySelectorAll("button");
+
+for (const button of buttons) {
+    button.addEventListener('click', questionAnswerListener)
+}
--- src/generate.js
+++ src/generate.js
@@ -1,7 +1,42 @@
+import fs from 'node:fs/promises'
+import { parseLine } from './lib/parse.js';
+import { generateQuestionCategoryHtml, generateQuestionHtml } from './lib/html.js';
+
+
+const MAX_QUESTIONS_PER_CATEGORY = 100
+
 async function main() {
-  console.log('generating...');
+
+  // Búa til möppu ef hún er ekki til
+
+  const distPath = './dist';
+  fs.mkdir(distPath);
+
+
+  const content = await fs.readFile('./questions.csv', 'utf-8')
+
+
+  const lines = content.split("\n")
+
+  const questions = lines.map(parseLine);
+
+  const qualityHistoryQuestions = questions
+    .filter((q) => q && q.categoryNumber === "4" && q.quality === "3")
+    .slice(0, MAX_QUESTIONS_PER_CATEGORY);
+
+  console.log(qualityHistoryQuestions.length);
+
+  const questionsHtml = qualityHistoryQuestions.map(generateQuestionHtml).join('\n');
+
+  const output = generateQuestionCategoryHtml('Saga', questionsHtml);
+
+  const path = './dist/saga.html';
+
+  fs.writeFile(path, output, 'utf-8');
+
 }
 
 main().catch((error) => {
   console.error('error generating', error);
+
 });
